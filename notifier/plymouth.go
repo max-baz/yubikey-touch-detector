58fb1faa8bc787cbf37122d4a1db5534c3e3168b
Antoine 'toinux' Lesieur <antoine.lesieur@s3ns.io>
2026-03-01 14:42:48 +0100

    feat: plymouth

--- notifier/plymouth.go
+++ notifier/plymouth.go
@@ -1,105 +1,89 @@
 package notifier
 
 import (
+	"os/exec"
 	"sync"
-	"sync/atomic"
 
-	"github.com/esiqveland/notify"
 	"github.com/godbus/dbus/v5"
-	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
-// SetupPlymouthNotifier configures a notifier to show all touch requests with Plymouth
-func SetupPlymouthNotifier(notifiers *sync.Map) {
+const (
+	plymouthDest  = "org.freedesktop.Plymouth"
+	plymouthPath  = "/org/freedesktop/Plymouth"
+	plymouthIface = "org.freedesktop.Plymouth"
+)
+
+func SetupPlymouthNotifier(notifiers *sync.Map, fallbackExec bool) {
 	touch := make(chan Message, 10)
-	notifiers.Store("notifier/libnotify", touch)
+	notifiers.Store("notifier/plymouth", touch)
 
-	notification := notify.Notification{
-		AppName: "yubikey-touch-detector",
-		AppIcon: "yubikey-touch-detector",
-		Summary: "YubiKey is waiting for a touch",
-	}
-
-	conn, notifier, err := connectDBus(&notification.ReplacesID)
-	if err != nil {
-		log.Error("Cannot initialize desktop notifications: ", err)
-		return
-	}
-	defer conn.Close()
-	defer notifier.Close()
-
+	message := "YubiKey is waiting for a touch..."
 	activeTouchWaits := 0
 
-	for {
-		value := <-touch
+	// Try to get a D-Bus connection, but don't exit if it fails
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		log.Warn("Could not connect to System Bus, will use CLI fallback: ", err)
+	} else {
+		defer conn.Close()
+	}
+
+	for value := range touch {
 		if value == GPG_ON || value == U2F_ON || value == HMAC_ON {
 			activeTouchWaits++
 		}
 		if value == GPG_OFF || value == U2F_OFF || value == HMAC_OFF {
 			activeTouchWaits--
 		}
+
 		if activeTouchWaits > 0 {
-			id, err := notifier.SendNotification(notification)
-			if err != nil {
-				log.Error("Cannot show notification (will reconnect to DBUS): ", err)
-				notifier.Close()
-				conn.Close()
-				conn, notifier, err = connectDBus(&notification.ReplacesID)
-				if err != nil {
-					log.Error("Failed to reconnect: ", err)
-					continue
-				}
-				id, err = notifier.SendNotification(notification)
-				if err != nil {
-					log.Error("Cannot show notification after reconnect: ", err)
-					continue
-				}
-			}
-			atomic.CompareAndSwapUint32(&notification.ReplacesID, 0, id)
-		} else if id := atomic.LoadUint32(&notification.ReplacesID); id != 0 {
-			if _, err := notifier.CloseNotification(id); err != nil {
-				log.Error("Cannot close notification (will reconnect to DBUS): ", err)
-				notifier.Close()
-				conn.Close()
-				conn, notifier, err = connectDBus(&notification.ReplacesID)
-				if err != nil {
-					log.Error("Failed to reconnect: ", err)
-				}
-			}
+			showPlymouthMessage(conn, message)
+		} else {
+			hidePlymouthMessage(conn, message)
 		}
 	}
 }
 
-func connectDBus(replacesID *uint32) (*dbus.Conn, notify.Notifier, error) {
-	conn, err := dbus.SessionBusPrivate()
-	if err != nil {
-		return nil, nil, errors.Wrapf(err, "unable to create session bus")
+// showPlymouthMessage attempts D-Bus first, then falls back to exec
+func showPlymouthMessage(conn *dbus.Conn, msg string) {
+	if conn != nil {
+		obj := conn.Object(plymouthDest, dbus.ObjectPath(plymouthPath))
+		call := obj.Call(plymouthIface+".DisplayMessage", 0, msg)
+		if call.Err == nil {
+			return // Success
+		}
+		log.Warn("D-Bus DisplayMessage failed, trying CLI: ", call.Err)
 	}
 
-	if err := conn.Auth(nil); err != nil {
-		conn.Close()
-		return nil, nil, errors.Wrapf(err, "unable to authenticate")
+	// Fallback: plymouth display-message --text="msg"
+	cmd := exec.Command("plymouth", "display-message", "--text", msg)
+	if err := cmd.Run(); err != nil {
+		log.Error("Plymouth CLI fallback failed: ", err)
 	}
-
-	if err := conn.Hello(); err != nil {
-		conn.Close()
-		return nil, nil, errors.Wrapf(err, "unable get bus name")
-	}
-
-	reset := func(msg *notify.NotificationClosedSignal) {
-		atomic.CompareAndSwapUint32(replacesID, msg.ID, 0)
-	}
-
-	notifier, err := notify.New(
-		conn,
-		notify.WithOnClosed(reset),
-		notify.WithLogger(log.StandardLogger()),
-	)
-	if err != nil {
-		conn.Close()
-		return nil, nil, errors.Wrapf(err, "unable to initialize D-Bus notifier interface")
-	}
-
-	return conn, notifier, nil
 }
+
+// hidePlymouthMessage attempts D-Bus first, then falls back to exec
+func hidePlymouthMessage(conn *dbus.Conn, msg string) {
+	if conn != nil {
+		obj := conn.Object(plymouthDest, dbus.ObjectPath(plymouthPath))
+		call := obj.Call(plymouthIface+".HideMessage", 0, msg)
+		if call.Err == nil {
+			return // Success
+		}
+		log.Warn("D-Bus HideMessage failed, trying CLI: ", call.Err)
+	}
+
+	// Fallback: plymouth hide-message --text="msg"
+	cmd := exec.Command("plymouth", "hide-message", "--text", msg)
+	if err := cmd.Run(); err != nil {
+		log.Error("Plymouth CLI hide fallback failed: ", err)
+	}
+}
+
+// Post-Boot-Stage
+
+// ❯ ./result/bin/yubikey-touch-detector --no-socket --plymouth
+
+// WARN[2026-03-01T00:18:21+01:00] Plymouth DisplayMessage failed (is Plymouth running?): The name org.freedesktop.Plymouth was not provided by any .service files
+// WARN[2026-03-01T00:18:28+01:00] Plymouth HideMessage failed: The name org.freedesktop.Plymouth was not provided by any .service files
